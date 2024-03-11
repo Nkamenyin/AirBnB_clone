5be3494bfdd9d6bdba8cae9015a3834071214695
Nkamenyin <valentineeyet01@gmail.com>
2024-03-11 17:18:03 +0100

    air

--- test1.py
+++ test1.py
@@ -1,12 +1,12 @@
-# Beautiful Python code
+#!/usr/bin/python3
 def greet_user(name):
     """Print a friendly greeting."""
     print(f"Hello, {name}! Welcome to the world of Python.")
 
-# Main program
-if __name__ == "__main__":
-    # Get user input for their name
-    user_name = input("Enter your name: ")
+    # Main program
+    if __name__ == "__main__":
 
-    # Call the greet_user function with the user's name
-    greet_user(user_name)
+        # Get user input for their name
+        user_name = input("Enter your name: ")
+        # Call the greet_user function with the user's name
+        greet_user(user_name)
